9068508c777b9624ba65a8d91369747154036f12
Mega <yossef.ayman1366@gmail.com>
2025-01-08 10:35:01 +0200

    game

--- scripts/jumper.gd
+++ scripts/jumper.gd
@@ -3,11 +3,11 @@ extends AnimatableBody2D
 @onready var jAnimate: AnimatedSprite2D = $jAnimate
 
 func _on_area_2d_body_entered(body):
-	if body is CharacterBody2D:
+	if body.name == "Player":
 		jAnimate.play("jump")
-		body.velocity.y = -600
+		body.velocity.y = -650
 
 func _on_area_2d_body_exited(body):
-	if body is CharacterBody2D:
+	if body.name == "Player":
 		jAnimate.play("idle")
 	
